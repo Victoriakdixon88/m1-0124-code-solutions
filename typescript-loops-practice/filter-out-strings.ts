f7770738d457131a0f8202f09d709bc011836c64
Victoria Dixon <victoriakdixon88@gmail.com>
2024-02-10 20:58:27 +0000

    completed typescript loops practice

--- typescript-loops-practice/filter-out-strings.ts
+++ typescript-loops-practice/filter-out-strings.ts
@@ -2,9 +2,9 @@
 function filterOutStrings(values:unknown[]):unknown[]{
   let filteredStrings:unknown[]=[];
   for(let i =0; i<values.length;i++ ){
-    if(typeof values[i] !=='string'){ //we need to use a typeof? why??
+    if(typeof values[i] !=='string'){ //we need to use a typeof? is it
       filteredStrings.push(values[i]);
-    }
-  }
+    };
+  };
   return filteredStrings;
 };
