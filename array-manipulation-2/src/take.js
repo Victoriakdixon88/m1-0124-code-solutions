90b3d825ad9541188a70a3e5f6b1d897a4a4c945
Victoria Dixon <victoriakdixon88@gmail.com>
2024-02-14 17:11:28 +0000

    completed array-manipulation-2

--- array-manipulation-2/src/take.js
+++ array-manipulation-2/src/take.js
@@ -1,14 +1,13 @@
 'use strict';
 /* exported take */
-function take(array, count) {
-  const newArray = [];
-  for (let i = 0; i <= Array.length - 1; i++) {
-    if (i === count) {
-      break;
+function take(array: unknown[], count: number): unknown[] {
+  const taken: unknown[] = [];
+  if (array.length === 0) {
+    return taken;
+  } else {
+    for (let n = 0; n < count && n < array.length; n++) {
+      taken.push(array[n]);
     }
-    newArray.push(array[i]);
   }
-  return newArray;
+  return taken;
 }
-//keyword in a loop, when we hit the break statement, it will end the loop no matter where it is.
-//continue - breaks counter part, if its inside the if statement, it will automatically force the loop to go to the next iteration.
--- array-manipulation-2/src/take.ts
+++ array-manipulation-2/src/take.ts
@@ -1,15 +1,13 @@
 /* exported take */
 
-function take(array: unknown[], count: number): unknown[] {
-  const newArray: unknown[] = [];
-  for (let i = 0; i <= Array.length - 1; i++) {
-    if (i === count) {
-      break;
+function take(array: any[], count: number): any[] {
+  const newArray: any[] = [];
+  if (array.length) {
+    for (let i = 0; i < count; i++) {
+      newArray.push(array[i]);
     }
-    newArray.push(array[i]);
+    return newArray;
+  } else {
+    return array;
   }
-  return newArray;
 }
-
-// keyword in a loop, when we hit the break statement, it will end the loop no matter where it is.
-// continue - breaks counter part, if its inside the if statement, it will automatically force the loop to go to the next iteration.
